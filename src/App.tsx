a03a2329328942aeccef2fbb0b10e08fa929ab7d
Benedikt Beigang <benedikt.beigang@stud.htwk-leipzig.de>
2024-03-31 22:04:43 +0200

    added building names

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import {
   MapContainer,
   TileLayer,
-  GeoJSON,
-  useMapEvents,
-  Rectangle,
-  Popup,
-  Marker,
+  GeoJSON
 } from "react-leaflet";
 import L, { Map } from "leaflet";
 import { HTWK_YELLOW } from "./Color";
 import { PublicTransport } from "./PublicTransport";
-import nieper from "./Assets/Nieper.png";
-import lipsius from "./Assets/Lipsius.png";
+import { Building } from "./Building";
+// import nieper from "./Assets/Abbreviation/Nieper.png";
+// import lipsius from "./Assets/Abbreviation/Lipsius.png";
+// import geutebrueck from "./Assets/Abbreviation/Geutebrueck.png";
+// import foeppl from "./Assets/Abbreviation/Foeppl.png";
+// import eichendorfstrasse from "./Assets/Abbreviation/Eichendorfstrasse.png";
+// import htwkBuildingMarker from "./Assets/htwkBuildingMarker";
 
 const START_ZOOM = 17 as const;
-const MAX_ZOOM = 19 as const;
+const MAX_ZOOM = 18 as const;
 const CENTER_OF_MAP: L.LatLngExpression = L.latLng(51.313, 12.374);
 
-const boundsOfTitle = L.latLngBounds([51.3147, 12.3744], [51.3138, 12.3765]);
-
-function ZoomHandler({
-  setFontSize,
-}: {
-  setFontSize: React.Dispatch<React.SetStateAction<number>>;
-}) {
-  const map = useMapEvents({
-    zoomend: () => {
-      const zoomLevel: number = map.getZoom();
-      const calc = 50 - (START_ZOOM - zoomLevel) * 50;
-      setFontSize(calc);
-      console.log("Zoom Level: " + zoomLevel + " Font Size: " + calc);
-    },
-  });
-
-  return null;
-}
-
 function App() {
-  const [fontSize, setFontSize] = useState(50);
-
   function style() {
     return {
       color: HTWK_YELLOW,
@@ -53,9 +33,6 @@ function App() {
         console.log("You clicked Building: " + feature.properties.Name);
       },
     });
-    // text to every building that is always visible and is customizable
-    // const popupContent = `<b>${feature.properties.Name}</b>`;
-    // layer.bindPopup(popupContent);
   }
 
   const mapRef = React.useRef<Map>(null);
@@ -68,30 +45,22 @@ function App() {
       maxZoom={MAX_ZOOM}
       ref={mapRef}
     >
-      <ZoomHandler setFontSize={setFontSize} />
 
-      <GeoJSON
-        data={require("./Assets/htwkBuildings.js")}
-        style={style()}
-        onEachFeature={onEachFeature}
-      />
+    <GeoJSON
+      data={require("./Assets/htwkBuildings.js")}
+      style={style()}
+      onEachFeature={onEachFeature}
+    />
 
-      <PublicTransport zoom={mapRef.current?.getZoom() ?? START_ZOOM} />
-      <Marker
-        position={new L.LatLng(51.313231581288704, 12.37287883331203)}
-        icon={new L.Icon({ iconUrl: nieper, iconSize: [25, 25] })}
-      ></Marker>
-      <Marker
-        position={new L.LatLng(51.31319879585476, 12.373778157922914)}
-        icon={new L.Icon({ iconUrl: lipsius, iconSize: [25, 25] })}
-      ></Marker>
+    <PublicTransport zoom={mapRef.current?.getZoom() ?? START_ZOOM} />
+    <Building />
 
-      <TileLayer
-        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        maxNativeZoom={MAX_ZOOM}
-        maxZoom={MAX_ZOOM}
-      />
+    <TileLayer
+      attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+      maxNativeZoom={MAX_ZOOM}
+      maxZoom={MAX_ZOOM}
+    />
     </MapContainer>
   );
 }
--- src/Building.tsx
+++ src/Building.tsx
@@ -1,19 +1,73 @@
-import React from 'react';
-import './Building.css';
+import { Marker } from 'react-leaflet';
+import L from 'leaflet';
 
-interface BuildingProperties {
-  onClick: () => void;
-  path: string;
+import nieper from "./Assets/Abbreviation/Nieper.png";
+import lipsius from "./Assets/Abbreviation/Lipsius.png";
+import geutebrueck from "./Assets/Abbreviation/Geutebrueck.png";
+import foeppl from "./Assets/Abbreviation/Foeppl.png";
+import eichendorfstrasse from "./Assets/Abbreviation/Eichendorfstrasse.png";
+import gutenberg from "./Assets/Abbreviation/Gutenberg.png";
+import medienzentrum from "./Assets/Abbreviation/Medienzentrum.png";
+import bibliothek from "./Assets/Abbreviation/Bibliothek.png";
+import zuse from "./Assets/Abbreviation/Zuse.png";
+import treffts_a from "./Assets/Abbreviation/Treffts_A.png";
+import treffts_b from "./Assets/Abbreviation/Treffts_B.png";
+import treffts_c from "./Assets/Abbreviation/Treffts_C.png";
+import treffts_l from "./Assets/Abbreviation/Treffts_L.png";
+
+function BuildingAbbreviation(lat: number, lng: number, iconUrl: string, iconSize: [number, number] = [25, 25]) {
+  return (
+    <Marker
+      position={new L.LatLng(lat, lng)}
+      icon={new L.Icon({ iconUrl: iconUrl, iconSize: iconSize })}
+    />
+  );
 }
 
-const Building: React.FC<BuildingProperties> = ({ onClick, path }) => (
-  <svg onClick={onClick}>
-    <g>
-      <path
-        d = {path}
-        fill="#ff3333"/>
-    </g>
-  </svg>
-);
-
-export default Building;
+export const Building = () => {
+  return (
+    <div>
+      {BuildingAbbreviation(51.313231581288704, 12.37287883331203, nieper)}
+      {BuildingAbbreviation(51.31319879585476, 12.373778157922914, lipsius)}
+      {BuildingAbbreviation(51.31461006223731, 12.372551384319365, geutebrueck)}
+      {BuildingAbbreviation(51.31434284662663, 12.372148540517315, foeppl)}
+      {BuildingAbbreviation(
+        51.31362514606505,
+        12.372034291663653,
+        eichendorfstrasse
+      )}
+      {BuildingAbbreviation(51.31243116113731, 12.374981180000901, gutenberg)}
+      {BuildingAbbreviation(
+        51.31238198598396,
+        12.374326540797853,
+        medienzentrum
+      )}
+      {BuildingAbbreviation(51.312492377572426, 12.373720461637419, bibliothek)}
+      {BuildingAbbreviation(51.312444708125014, 12.375968173281649, zuse)}
+      {BuildingAbbreviation(
+        51.31284,
+        12.375638756225442,
+        treffts_a,
+        [50, 25]
+      )}
+      {BuildingAbbreviation(
+        51.3132261337642,
+        12.376044146921913,
+        treffts_b,
+        [50, 25]
+      )}
+      {BuildingAbbreviation(
+        51.31346,
+        12.375919720074506,
+        treffts_c,
+        [50, 25]
+      )}
+      {BuildingAbbreviation(
+        51.313119597020204,
+        12.375229639323209,
+        treffts_l,
+        [50, 25]
+      )}
+    </div>
+  );
+};
