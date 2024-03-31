dc3330bfa9bd86afb4a79700916baef81d45da76
Benedikt Beigang <benedikt.beigang@stud.htwk-leipzig.de>
2024-03-31 20:21:57 +0200

    added routes of public transportation

--- src/Color.ts
+++ src/Color.ts
@@ -1,1 +1,4 @@
 export const HTWK_YELLOW = "#ffed00" as const;
+export const LEIPZIG_YELLOW = "#ffcc00" as const;
+export const LEIPZIG_RED = "#e30613" as const;
+export const LEIPZIG_PURPLE = "#a71681" as const;
--- src/PublicTransport.tsx
+++ src/PublicTransport.tsx
@@ -1,5 +1,7 @@
-import { Marker } from "react-leaflet";
-import L, { Path } from "leaflet";
+import { Marker, GeoJSON, FeatureGroup } from "react-leaflet";
+import L from "leaflet";
+import { LEIPZIG_RED, LEIPZIG_YELLOW, LEIPZIG_PURPLE } from "./Color";
+
 import htwk_north from "./Assets/HTWK_North.png";
 import htwk_east from "./Assets/HTWK_East.png";
 import connewitz_south from "./Assets/Connewitz_South.png";
@@ -11,53 +13,95 @@ type PublicTransportProps = {
   zoom: number;
 };
 
+function style(color: string, zoom: number) {
+  const opacity = OPACITY_ZOOM_LEVEL <= zoom ? 1 : 0 as const;
+  switch (color) {
+    case "red":
+      return { color: LEIPZIG_RED, opacity: opacity};
+    case "yellow":
+      return { color: LEIPZIG_YELLOW, opacity: opacity };
+    case "purple":
+      return { color: LEIPZIG_PURPLE, opacity: opacity };
+    default:
+      return { color: "#ffffff", opacity: opacity };
+  }
+}
+
+function publicTransportStop(
+  position: L.LatLng, 
+  iconUrl: string, 
+  iconSize: [number, number], 
+  iconAnchor: [number, number], 
+  zoom: number){
+  return (
+    <Marker
+      position={position}
+      icon={
+        new L.Icon({
+          iconUrl: iconUrl,
+          iconSize: iconSize,
+          iconAnchor: iconAnchor,
+        })
+      }
+      opacity={OPACITY_ZOOM_LEVEL <= zoom ? 1 : 0}
+    />
+  );
+}
+
 export const PublicTransport: React.FC<PublicTransportProps> = ({ zoom }) => {
   return (
     <div>
-      <Marker
-        position={new L.LatLng(51.31535, 12.37335)}
-        icon={
-          new L.Icon({
-            iconUrl: htwk_north,
-            iconSize: [40, 84],
-            iconAnchor: [20, 42],
-          })
-        }
-        opacity={OPACITY_ZOOM_LEVEL <= zoom ? 1 : 0}
-      />
-      <Marker
-        position={new L.LatLng(51.31488, 12.3741)}
-        icon={
-          new L.Icon({
-            iconUrl: htwk_east,
-            iconSize: [84, 40],
-            iconAnchor: [42, 20],
-          })
-        }
-        opacity={OPACITY_ZOOM_LEVEL <= zoom ? 1 : 0}
-      />
-      <Marker
-        position={new L.LatLng(51.3114, 12.37323)}
-        icon={
-          new L.Icon({
-            iconUrl: connewitz_south,
-            iconSize: [40, 172],
-            iconAnchor: [20, 20],
-          })
-        }
-        opacity={OPACITY_ZOOM_LEVEL <= zoom ? 1 : 0}
-      />
-      <Marker
-        position={new L.LatLng(51.31165, 12.3727)}
-        icon={
-          new L.Icon({
-            iconUrl: connewitz_west,
-            iconSize: [40, 40],
-            iconAnchor: [20, 20],
-          })
-        }
-        opacity={OPACITY_ZOOM_LEVEL <= zoom ? 1 : 0}
-      />
+      {publicTransportStop(
+        new L.LatLng(51.31535, 12.37335),
+        htwk_north,
+        [40, 84],
+        [20, 42],
+        zoom
+      )}
+      {publicTransportStop(
+        new L.LatLng(51.31488, 12.3741),
+        htwk_east,
+        [84, 40],
+        [42, 20],
+        zoom
+      )}
+      {publicTransportStop(
+        new L.LatLng(51.3114, 12.37323),
+        connewitz_south,
+        [40, 172],
+        [20, 20],
+        zoom
+      )}
+      {publicTransportStop(
+        new L.LatLng(51.31165, 12.3727),
+        connewitz_west,
+        [40, 40],
+        [20, 20],
+        zoom
+      )}
+
+      <FeatureGroup>
+        <GeoJSON
+          data={require("./Assets/Routes/Bus70_Route.js")}
+          style={style("purple", zoom)}
+        />
+        <GeoJSON
+          data={require("./Assets/Routes/Bus89_Route.js")}
+          style={style("purple", zoom)}
+        />
+        <GeoJSON
+          data={require("./Assets/Routes/Tram9_Route.js")}
+          style={style("yellow", zoom)}
+        />
+        <GeoJSON
+          data={require("./Assets/Routes/Tram10_Route.js")}
+          style={style("red", zoom)}
+        />
+        <GeoJSON
+          data={require("./Assets/Routes/Tram11_Route.js")}
+          style={style("red", zoom)}
+        />
+      </FeatureGroup>
     </div>
   );
 };
