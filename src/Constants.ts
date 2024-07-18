e340a6360e683cf2c54b1637583eda10944b984e
Benedikt Beigang <benedikt.beigang@stud.htwk-leipzig.de>
2024-07-18 22:29:43 +0200

    HB_0-4

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,1 +1,1 @@
-export const FinishedBuildings = ["ZU", "GU", "MZ", "TR_L", "TR_A", "TR_B", "TR_C", "FE"] as string[];
+export const FinishedBuildings = ["ZU", "GU", "MZ", "TR_L", "TR_A", "TR_B", "TR_C", "FE", "HB"] as string[];
