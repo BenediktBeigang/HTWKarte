9d7e0f8a9b8bd87621bc0a1582b2c0682d1bb073
Benedikt Beigang <benedikt.beigang@stud.htwk-leipzig.de>
2025-04-26 22:23:57 +0200

    fixed web-vitals deprecated calls

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,14 @@
-import { ReportHandler } from "web-vitals";
+import type { Metric } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void): void => {
+  if (typeof onPerfEntry === "function") {
+    import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB, onINP }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+      onINP(onPerfEntry);
     });
   }
 };
